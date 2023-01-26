3ffce9b7fe2e5aeab8018ad0f733359078b97815
Eduardo Arroyo Morales <digiespirit.1807@gmail.com>
2023-01-26 15:21:34 -0600

    56 - Tipos de request

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,3 +3,78 @@
 @section('titulo')
     Registrate en DevStagram
 @endsection
+
+@section('contenido')
+    <div class="md:flex md:justify-center md:gap-10 md:items-center">
+        <div class="md:w-6/12 p-5">
+            <img src="{{asset('img/registrar.jpg')}}" alt="Imagen para presentar el registro">
+        </div>
+        <div class="md:w-4/12 bg-white p-6 rounded-lg shadow-xl">
+            <form action="/crear-cuenta" method="POST">
+                <div class="mb-5">
+                    <label for="name" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Nombre
+                    </label>
+                    <input 
+                        id="name" 
+                        name="name" 
+                        type="text"
+                        placeholder="Tu nombre" 
+                        class="border-2 border-gray-200 p-2 w-full rounded-lg"
+                    />
+                </div>
+                <div class="mb-5">
+                    <label for="username" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Nombre de usuario
+                    </label>
+                    <input 
+                        id="username" 
+                        name="username" 
+                        type="text"
+                        placeholder="Tu nombre de usuario" 
+                        class="border-2 border-gray-200 p-2 w-full rounded-lg"
+                    />
+                </div>
+                <div class="mb-5">
+                    <label for="email" class="mb-2 block uppercase text-gray-500 font-bold">
+                        E-mail
+                    </label>
+                    <input 
+                        id="email" 
+                        name="email" 
+                        type="email"
+                        placeholder="Tu correo electrónico de registro" 
+                        class="border-2 border-gray-200 p-2 w-full rounded-lg"
+                    />
+                </div>
+                <div class="mb-5">
+                    <label for="password" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Contraseña
+                    </label>
+                    <input 
+                        id="password" 
+                        name="password" 
+                        type="password"
+                        placeholder="Tu contraseña de registro" 
+                        class="border-2 border-gray-200 p-2 w-full rounded-lg"
+                    />
+                </div>
+                <div class="mb-5">
+                    <label for="password_confirmation" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Repetir Contraseña
+                    </label>
+                    <input 
+                        id="password_confirmation" 
+                        name="password_confirmation" 
+                        type="password"
+                        placeholder="Repite tu contraseña de registro" 
+                        class="border-2 border-gray-200 p-2 w-full rounded-lg"
+                    />
+                </div>
+                <input 
+                    type="submit"
+                />
+            </form>
+        </div>
+    </div>
+@endsection
