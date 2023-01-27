47f5f9ded5223c550ecd9970921a8fcf2c8a3426
Eduardo Arroyo Morales <digiespirit.1807@gmail.com>
2023-01-27 16:38:33 -0600

    92 - Instalando dropzone

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,21 @@
-import './bootstrap';
+import Dropzone from "dropzone";
+
+Dropzone.autoDiscover = false;
+
+const dropzone = new Dropzone("#dropzone", 
+{
+    dictDefaultMessage: "Sube aquí tu imagen",
+    acceptedFiles: ".jpg, .jpeg, .png, .gif",
+    addRemoveLinks: true,
+    dictRemoveFile: "Eliminar imagen",
+    maxFiles: 1,
+    uploadMultiple: false,
+    // url: "/upload",
+    // paramName: "file",
+    // maxFilesize: 2,
+    // headers: {
+    //     "X-CSRF-TOKEN": document
+    //         .querySelector('meta[name="csrf-token"]')
+    //         .getAttribute("content")
+    // }
+});
