658a32f6be1cd273ae8c6b8beb6800740230196d
Eduardo Arroyo Morales <digiespirit.1807@gmail.com>
2023-01-27 14:21:11 -0600

    85 - URL's únicas para los módulos

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -13,10 +14,11 @@ class PostController extends Controller
         $this->middleware('auth');
     }
 
-    public function index()
+    public function index(User $user)
     {
         // dd('Hola desde el controlador de Post');
         // dd(auth()->user());
+        // dd($user->username);
         return view('dashboard');
     }
 }
