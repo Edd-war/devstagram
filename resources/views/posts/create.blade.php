e3bf6fac31b43ace6b8a3887878e7928cd7e7bca
Eduardo Arroyo Morales <digiespirit.1807@gmail.com>
2023-01-27 16:12:39 -0600

    90 - Finalizando el formulario

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -15,23 +15,44 @@
             <form action="{{ route('register') }}" method="POST" novalidate>
                 @csrf
                 <div class="mb-5">
-                    <label for="name" class="mb-2 block uppercase text-gray-500 font-bold">
-                        Nombre
+                    <label for="titulo" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Título
                     </label>
                     <input 
-                        id="name" 
-                        name="name" 
+                        id="titulo" 
+                        name="titulo" 
                         type="text"
-                        placeholder="Tu nombre" 
-                        class="border-2 border-gray-200 p-2 w-full rounded-lg @error('name') border-red-500 @enderror"
-                        value="{{old('name')}}"
+                        placeholder="Título de la publicación" 
+                        class="border-2 border-gray-200 p-2 w-full rounded-lg @error('titulo') border-red-500 @enderror"
+                        value="{{old('titulo')}}"
                     />
-                    @error('name')
+                    @error('titulo')
                         <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">
                             {{$message}}
                         </p>
                     @enderror
                 </div>
+                <div class="mb-5">
+                    <label for="descripcion" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Descripción
+                    </label>
+                    <textarea 
+                        id="descripcion" 
+                        name="descripcion" 
+                        placeholder="Descripción de la publicación" 
+                        class="border-2 border-gray-200 p-2 w-full rounded-lg @error('descripcion') border-red-500 @enderror"
+                    >{{old('descipcion')}}</textarea>
+                    @error('descripcion')
+                        <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">
+                            {{$message}}
+                        </p>
+                    @enderror
+                </div>
+                <input 
+                    type="submit"
+                    value="Publicar"
+                    class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg"
+                />
             </form>
         </div>
     </div>
