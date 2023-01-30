251d8dcb9a939ee8961763f971842d3d74fb771c
Eduardo Arroyo Morales <digiespirit.1807@gmail.com>
2023-01-30 15:24:44 -0600

    102 - Ejecutar y comprobar el factory

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -20,7 +20,7 @@ class PostFactory extends Factory
             'titulo' => $this->faker->sentence(5),
             'contenido' => $this->faker->sentence(20),
             'imagen' => $this->faker->uuid() . '.jpg',
-            'user_id' => $this->faker->randomElement([1, 2, 3]),
+            'user_id' => $this->faker->randomElement([1, 2, 3, 4, 5]),
             // 'imagen' => 'posts/' . $this->faker->image('public/storage/posts', 640, 480, null, false),
             // 'user_id' => \App\Models\User::all()->random()->id,
         ];
