36633b1eec88c8098efb120f5055171100c5b8d6
Eduardo Arroyo Morales <digiespirit.1807@gmail.com>
2023-01-31 10:32:11 -0600

    117 - Filtrar por Colección

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -33,8 +33,10 @@
 
     <section class="container mx-auto mt-10">
         <h2 class="text-4xl text-center font-black my-10">Publicaciones</h2>
+        {{-- {{ dd($user->posts) }} --}}
         {{-- {{dd($posts->count())}} --}}
-        @if ($posts->count())            
+        {{-- @if ($user->posts as $post) --}}
+        @if ($posts->count())
             <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
                 @foreach ($posts as $post)
                 <div>
