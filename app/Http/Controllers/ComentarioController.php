3f5f8672ebd3f8b35c88f1a181f19214bf9fb681
Eduardo Arroyo Morales <digiespirit.1807@gmail.com>
2023-01-31 12:48:52 -0600

    125 - Almacenando los comentarios

--- app/Http/Controllers/ComentarioController.php
+++ app/Http/Controllers/ComentarioController.php
@@ -2,13 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
+use App\Models\User;
+use App\Models\Comentario;
 use Illuminate\Http\Request;
 
 class ComentarioController extends Controller
 {
     //
-    public function store()
+    public function store(Request $request, User $user, Post $post)
     {
-        dd('Creando comentario...');
+        // dd('Creando comentario...');
+        // VALIDAR DATOS
+        $this->validate(request(), [
+            'comentario' => 'required|max:255'
+        ]);
+
+        // GUARDAR DATOS
+        Comentario::create([
+            'comentario' => $request->comentario,
+            'user_id' => auth()->user()->id,
+            'post_id' => $post->id
+        ]);
+
+        // REDIRECCIONAR
+        return back()->with('mensaje', 'Comentario agregado');
     }
 }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -32,33 +32,38 @@
             <div class="shadow bg-white p-5 mb-5">
                 @auth
                     
-                <p class="text-xl font-bold text-center mb-4">
-                    Agrega un nuevo comentario
-                </p>
-                <form action="{{ route('comentarios.store', ['post' => $post, 'user' => $user]) }}" method="POST">
-                    @csrf
-                    <div class="mb-5">
-                        <label for="comentario" class="mb-2 block uppercase text-gray-500 font-bold">
-                            Añade un comentario
-                        </label>
-                        <textarea 
-                        id="comentario" 
-                        name="comentario" 
-                        placeholder="Agrega un comentario" 
-                        class="border-2 border-gray-200 p-2 w-full rounded-lg @error('descripcion') border-red-500 @enderror"
-                        ></textarea>
-                        @error('comentario')
-                        <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">
-                            {{$message}}
-                        </p>
-                        @enderror
-                    </div>
-                    <input 
-                    type="submit"
-                    value="Comentar esta forma"
-                    class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg"
-                    />
-                </form>
+                    <p class="text-xl font-bold text-center mb-4">
+                        Agrega un nuevo comentario
+                    </p>
+                    @if (session('mensaje'))
+                        <div class="bg-green-500 p-2 rounded-lg mb-6 text-white text-center uppercase font-bold">
+                            {{ session('mensaje') }}
+                        </div>
+                    @endif
+                    <form action="{{ route('comentarios.store', ['post' => $post, 'user' => $user]) }}" method="POST">
+                        @csrf
+                        <div class="mb-5">
+                            <label for="comentario" class="mb-2 block uppercase text-gray-500 font-bold">
+                                Añade un comentario
+                            </label>
+                            <textarea 
+                            id="comentario" 
+                            name="comentario" 
+                            placeholder="Agrega un comentario" 
+                            class="border-2 border-gray-200 p-2 w-full rounded-lg @error('descripcion') border-red-500 @enderror"
+                            ></textarea>
+                            @error('comentario')
+                            <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">
+                                {{$message}}
+                            </p>
+                            @enderror
+                        </div>
+                        <input 
+                        type="submit"
+                        value="Comentar esta forma"
+                        class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg"
+                        />
+                    </form>
                 @endauth
             </div>
         </div>
