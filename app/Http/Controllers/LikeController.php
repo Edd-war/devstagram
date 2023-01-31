6e74e322ccecd9f63a4ea0338672426fd0461e3e
Eduardo Arroyo Morales <digiespirit.1807@gmail.com>
2023-01-31 16:13:40 -0600

    134 - Guardando el Like del Usuario

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class LikeController extends Controller
 {
     //
+    public function store(Request $request, Post $post)
+    {
+        // dd('like');
+        // dd($request->user()->id);
+        $post->likes()->create([
+            'user_id' => $request->user()->id
+        ]);
+
+        return back();
+    }
 }
