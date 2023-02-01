e89239bdbe3f0d1d2724be67f224f6ca2e75b1c3
Eduardo Arroyo Morales <digiespirit.1807@gmail.com>
2023-02-01 16:50:47 -0600

    152 - Añadir Strings para mostrar los textos de seguidores correctamente

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -35,8 +35,8 @@
                     @endauth
                 </div>
                 <p class="text-gray-800 text-sm mb-3 font-bold">
-                    0
-                    <span class="font-normal"> Seguidores</span>
+                    {{ $user->followers->count() }}
+                    <span class="font-normal"> @choice('seguidor|Seguidores', $user->followers->count())</span>
                 </p>
                 <p class="text-gray-800 text-sm mb-3 font-bold">
                     0
