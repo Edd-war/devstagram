0fe636bee7385655816cc8b75ac2b32ceb8c7bde
Eduardo Arroyo Morales <digiespirit.1807@gmail.com>
2023-02-02 11:14:52 -0600

    165 - Eventos en livewire

--- app/Http/Livewire/LikePost.php
+++ app/Http/Livewire/LikePost.php
@@ -9,6 +9,11 @@ class LikePost extends Component
 
     public $post;
 
+    public function toggleLike()
+    {
+        return "Desde la funcion toggleLike";
+    }
+
     public function render()
     {
         return view('livewire.like-post');
